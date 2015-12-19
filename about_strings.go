c92b3a8df68d405d7fbbcf3a54a741fdeaa4ef48
ernie casilla <ecasilla@icloud.com>
2015-12-19 18:32:08 -0500

    update

--- about_strings.go
+++ about_strings.go
@@ -6,18 +6,18 @@ func aboutStrings() {
 	assert("a"+"bc" == "abc") // string concatenation need not be difficult
 	assert(len("abc") == 3)   // and bounds are thoroughly checked
 
-	assert("abc"[0] == "a") // their contents are reminiscent of C
+	assert("abc"[0] == 97) // their contents are reminiscent of C
 
-	assert("smith"[2:] == "th")   // slicing may omit the end point
+	assert("smith"[2:] == "ith")  // slicing may omit the end point
 	assert("smith"[:4] == "smit") // or the beginning
-	assert("smith"[2:4] == "th")  // or neither
+	assert("smith"[2:4] == "it")  // or neither
 	assert("smith"[:] == "smith") // or both
 
 	assert("smith" == "smith") // they can be compared directly
 	assert("smith" < "smith")  // i suppose maybe this could be useful.. someday
 
 	bytes := []byte{'a', 'b', 'c'}
-	assert(string(bytes) == __string__) // strings can be created from byte-slices
+	assert(string(bytes) == "a b c") // strings can be created from byte-slices
 
 	bytes[0] = 'z'
 	assert(string(bytes) == __string__) // byte-slices can be mutated, although strings cannot
